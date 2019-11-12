33f32a5a04c72591f49f51bbb1b9ae7a7eb62399
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-11-12 13:29:55 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
 import './App.css';
 
+const Home = () => (
+  <h1>Hello guys! Let's deal with 404?</h1>
+);
+
+const About = () => (
+<h1>About this example....</h1>
+)
+
+const Page404 = ({ location }) => (
+<div>
+  <h2>No match found for <code>{location.pathname}</code></h2>
+</div>
+);
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <div className="App">
+        <BrowserRouter>
+          <div>
+            <p className="App-intro">
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/more">More</Link></li>
+              </ul>
+            </p>
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/about" component={About}/>
+              <Route component={Page404} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
   );
 }
 
